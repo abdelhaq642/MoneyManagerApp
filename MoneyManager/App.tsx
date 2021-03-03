badb3e110b68aba0fd80e3e6f8536683b844693a
Hussein226 <41562653+Hussein226@users.noreply.github.com>
2021-03-03 10:53:50 -0500

    curly good morning

--- MoneyManager/App.tsx
+++ MoneyManager/App.tsx
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import DrawerComponent from "./Components/DrawerComponent";
+import LoginScreen from "./NavigationScreen/LoginScreen";
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <Stack.Navigator initialRouteName="Login_Screen">
+        <Stack.Screen name="Login_Screen" component={LoginScreen} />
+      </Stack.Navigator> */}
+
+      <DrawerComponent />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
